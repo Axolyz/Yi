4b89780fd55472c906df75c53eef47b9e37f0a85
Technetium <samuj0220@gmail.com>
2022-09-10 16:07:57 +0800

    1111

--- Yi.py
+++ Yi.py
@@ -18,10 +18,10 @@ def rem_minus(x):
     return x
 
 
-switch = ''
-while switch != 'q':
-    input('举意完毕后按回车继续。')
-    bos = []
+switch = ""
+while switch != "q":
+    input("举意完毕后按回车继续。")
+    yaos = []
     for c in range(6):
         n = 50
         n = n - 1
@@ -36,18 +36,18 @@ while switch != 'q':
             x = rem_minus(x)
             y = rem_minus(y)
             n = x + y
-        bo = int(n / 4)
-        bos.insert(0, bo)
+        yao = int(n / 4)
+        yaos.insert(0, yao)
     print("本卦：")
-    for bo in bos:
-        if bo == 6 or bo == 8:
+    for yao in yaos:
+        if yao == 6 or yao == 8:
             print("---   ---")
-        elif bo == 7 or bo == 9:
-            print('---------')
+        elif yao == 7 or yao == 9:
+            print("---------")
     print("之卦：")
-    for bo in bos:
-        if bo == 9 or bo == 8:
+    for yao in yaos:
+        if yao == 9 or yao == 8:
             print("---   ---")
-        elif bo == 7 or bo == 6:
-            print('---------')
-    switch = input('请查看程序所在目录下的卦名对照表自行解卦。\n输入q退出，输入任意值重来。')
+        elif yao == 7 or yao == 6:
+            print("---------")
+    switch = input("请查看程序所在目录下的卦名对照表自行解卦。\n输入q退出，输入任意值重来。")
